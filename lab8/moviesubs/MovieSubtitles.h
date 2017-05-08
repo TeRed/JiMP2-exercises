770b72e6ae19c3782ea83cbce16ab5bbd897ed94
Albert Duź <dddd.albert@gmail.com>
2017-05-08 15:19:08 +0200

    Lab 8 SubRip and MicroDvd

--- lab8/moviesubs/MovieSubtitles.h
+++ lab8/moviesubs/MovieSubtitles.h
@@ -4,32 +4,48 @@
 
 #ifndef JIMP_EXERCISES_MOVIESUBTITLES_H
 #define JIMP_EXERCISES_MOVIESUBTITLES_H
-
 #include <sstream>
 
 using ::std::stringstream;
 using ::std::string;
+using ::std::invalid_argument;
 
-namespace moviesubs {
+namespace moviesubs{
+    class MissingTimeSpecification {};
+    class OutOfOrderFrames {};
     class NegativeFrameAfterShift {};
     class InvalidSubtitleLineFormat {};
     class SubtitleEndBeforeStart {
     private:
-        int at_line;
-        string what_line;
+        int exception_line_number;
+        string exception_line_string;
     public:
-        SubtitleEndBeforeStart();
+        SubtitleEndBeforeStart(int line_number, string line_string);
         int LineAt() const;
         string what() const;
-        void inc();
-        void set_what(string what);
     };
 
-    class MicroDvdSubtitles{
+    class MovieSubtitles {
     public:
-        MicroDvdSubtitles(){};
-        void ShiftAllSubtitlesBy(int delay, int fps, stringstream *in, stringstream *out) throw
-        (NegativeFrameAfterShift, SubtitleEndBeforeStart, InvalidSubtitleLineFormat);
+        MovieSubtitles() = default;
+        virtual void ShiftAllSubtitlesBy(int, int fps, std::stringstream *in, std::stringstream *out) = 0;
     };
+
+    class MicroDvdSubtitles : public MovieSubtitles {
+    public:
+        MicroDvdSubtitles() = default;
+        virtual void ShiftAllSubtitlesBy(int, int fps, stringstream *in, stringstream *out) throw
+        (NegativeFrameAfterShift, SubtitleEndBeforeStart, InvalidSubtitleLineFormat, invalid_argument);
+    };
+
+    class SubRipSubtitles : public MovieSubtitles {
+    public:
+        SubRipSubtitles() = default;
+        virtual void ShiftAllSubtitlesBy(int delay, int fps, std::stringstream *in, std::stringstream *out) throw
+        (NegativeFrameAfterShift, SubtitleEndBeforeStart, InvalidSubtitleLineFormat, OutOfOrderFrames, invalid_argument);
+    };
+
+    //used by SubRipSubtitles::ShiftAllSubtitlesBy
+    void print_time(int miliseconds, char separators[], std::stringstream *out);
 }
 #endif //JIMP_EXERCISES_MOVIESUBTITLES_H
--- lab8/moviesubs/main.cpp
+++ lab8/moviesubs/main.cpp
@@ -1,16 +1,3 @@
 //
 // Created by albert on 02.05.17.
 //
-
-#include <iostream>
-#include "MovieSubtitles.h"
-
-int main() {
-    moviesubs::MicroDvdSubtitles ms;
-    stringstream in("{0}{250}TEXT TEXT\n{260}{300}NEWLINE\n");
-    stringstream out;
-    ms.ShiftAllSubtitlesBy(1000, 24, &in, &out);
-
-    std::cout << out.str();
-    return 0;
-}
